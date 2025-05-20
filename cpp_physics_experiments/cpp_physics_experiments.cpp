dfb22e2aeaa6df4d489db12412951addb3f1aa6c
sg-mcgee <garton@ou.edu>
2025-05-20 01:06:52 -0500

    Test fibonacci

--- cpp_physics_experiments/cpp_physics_experiments.cpp
+++ cpp_physics_experiments/cpp_physics_experiments.cpp
@@ -3,8 +3,19 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::cout << "Hello World!\n" << std::endl;
+
+    int fib1 = 1;
+    int fib2 = 1;
+    int fib3;
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Fibonacci value:" << fib1 << std::endl;
+        fib3 = fib1;
+        fib1 = fib1 + fib2;
+        fib2 = fib3;
+    }
+
     return 0;
 }
